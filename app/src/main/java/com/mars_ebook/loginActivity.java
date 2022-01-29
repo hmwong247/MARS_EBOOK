384651987f96ac0d662dc2fce68b69f1051c3c1d
karina1208 <56858132+karina1208@users.noreply.github.com>
2022-01-29 17:01:58 +0800

    Add login

---
+++ app/src/main/java/com/mars_ebook/loginActivity.java
@@ -0,0 +1,12 @@
+package com.mars_ebook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class loginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
