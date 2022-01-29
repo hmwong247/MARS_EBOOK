7d517ccc0eb2294d1c2defd9eae8a2f6111d44d3
karina1208 <56858132+karina1208@users.noreply.github.com>
2022-01-29 16:06:44 +0800

    Upload book and youtube link

---
+++ app/src/main/java/com/example/mars_ebook/UploadLinkActivity.java
@@ -0,0 +1,13 @@
+package com.example.mars_ebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadLinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_link);
+    }
+}
