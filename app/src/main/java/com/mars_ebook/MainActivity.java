f997361302ad7516d9ce7d3c3c90e8140bb82795
erickau2 <64601280+erickau2@users.noreply.github.com>
2022-01-30 01:59:19 +0800

    add ChapterActivity new main

--- app/src/main/java/com/mars_ebook/MainActivity.java
+++ app/src/main/java/com/mars_ebook/MainActivity.java
@@ -21,16 +21,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Chapter> ChapterList = new ArrayList<Chapter>();
-        ChapterList.add(new Chapter("1", "aa", "a1", 1, 1));
-        ChapterList.add(new Chapter("2", "bb", "a2", 1, 1));
-        ChapterList.add(new Chapter("3", "cc", "a3", 1, 1));
-        ChapterList.add(new Chapter("4", "dd", "a4", 1, 1));
-        ChapterList.add(new Chapter("5", "ee", "a5", 1, 1));
-
-        RecyclerView bookRecycler = (RecyclerView) findViewById(R.id.recycler_book);
-        bookRecycler.setLayoutManager(new LinearLayoutManager(this));
-        bookRecycler.setAdapter(new ChapterListAdapter(this, ChapterList));
+//        List<Chapter> ChapterList = new ArrayList<Chapter>();
+//        ChapterList.add(new Chapter("1", "aa", "a1", 1, 1));
+//        ChapterList.add(new Chapter("2", "bb", "a2", 1, 1));
+//        ChapterList.add(new Chapter("3", "cc", "a3", 1, 1));
+//        ChapterList.add(new Chapter("4", "dd", "a4", 1, 1));
+//        ChapterList.add(new Chapter("5", "ee", "a5", 1, 1));
+//
+//        RecyclerView bookRecycler = (RecyclerView) findViewById(R.id.recycler_book);
+//        bookRecycler.setLayoutManager(new LinearLayoutManager(this));
+//        bookRecycler.setAdapter(new ChapterListAdapter(this, ChapterList));
 
         final Button button = findViewById(R.id.loginButton);
         button.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/mars_ebook/accountActivity.java
+++ app/src/main/java/com/mars_ebook/accountActivity.java
@@ -18,5 +18,7 @@ public class accountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
 }
 }
