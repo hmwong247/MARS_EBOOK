78ac120809fc991a391eedad23a47c54a77a8275
erickau2 <64601280+erickau2@users.noreply.github.com>
2022-01-29 22:23:09 +0800

    fix loginfail in loginActivity, add accountActivity.java

---
+++ app/src/main/java/com/mars_ebook/accountActivity.java
@@ -0,0 +1,17 @@
+package com.mars_ebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class accountActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/mars_ebook/loginActivity.java
+++ app/src/main/java/com/mars_ebook/loginActivity.java
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,15 +21,23 @@ public class loginActivity extends AppCompatActivity {
         loginButton=(Button)findViewById(R.id.loginButton);
         username=(EditText) findViewById(R.id.editTextEmail);
         password=(EditText) findViewById(R.id.editTextPassword);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(username.getText().toString(),
+                        password.getText().toString());
+            }
+        });
     }
 
     private void validate(String username, String pw){
         if((username =="may")&&(pw == "123")){
-            Toast.makeText(getApplicationContext(),"Login success",Toast.LENGTH_SHORT).show();
-            //Intent intent =new Intent(MainActivity.this,.class,Account.class);
-            //startActivities(intent);
+            Toast.makeText(getApplicationContext(),"Login success",Toast.LENGTH_LONG).show();
+            //Intent intent =new Intent(MainActivity.this,.class,accountActivity.class);
+            //startActivity(intent);4
         }else{
-            Toast.makeText(getApplicationContext(),"Login fail",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Login fail",Toast.LENGTH_LONG).show();
         }
 
     }
